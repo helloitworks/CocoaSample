92a47f9b823e4cad1e99e0af05fc23177293f15f
Shen Yixin <sam@samtekiMac-mini.local>
2013-12-20 13:53:44 +0800

    SYXComplexTableViewWnd

---
+++ CocoaSample/Source/ComplexTableView/SYXComplexTableViewWndCtrl.h
@@ -0,0 +1,13 @@
+//
+//  SYXComplexTableViewWndCtrl.h
+//  CocoaSample
+//
+//  Created by Shen Yixin on 13-12-20.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SYXComplexTableViewWndCtrl : NSWindowController
+
+@end
---
+++ CocoaSample/Source/ComplexTableView/SYXComplexTableViewWndCtrl.m
@@ -0,0 +1,33 @@
+//
+//  SYXComplexTableViewWndCtrl.m
+//  CocoaSample
+//
+//  Created by Shen Yixin on 13-12-20.
+//
+//
+
+#import "SYXComplexTableViewWndCtrl.h"
+
+@interface SYXComplexTableViewWndCtrl ()
+
+@end
+
+@implementation SYXComplexTableViewWndCtrl
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
